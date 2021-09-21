aa86b2116aeaa3d3175e2f3c84dde08c7d1ab682
yongdoc <ayahyaa17@gmail.com>
2021-09-21 16:06:24 +0700

    add vue basic

--- frontend-vuejs/src/App.vue
+++ frontend-vuejs/src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="logo" src="./assets/yangfei.png" width="250" height="250">
+  <p>{{title}} {{visitor}}</p>
+  <button @click="visitor++">Increase</button>
+  <button @click="visitor--">Decrease</button>
+  <br>
+  <button @click="Show">
+    <span v-if="!accepted">Show it!</span>
+    <span v-if="accepted">Nah, no more</span>
+    </button>
+  <div v-if="accepted">
+    <p>What's your name ?</p>
+    <input type="text" name="name" v-model="change">
+    <button @click="ChangeName(change)">Submit</button>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -9,7 +22,23 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  methods: {
+    ChangeName(me){
+      this.title=me
+    },
+    Show(){
+      this.accepted = !this.accepted
+    }
+  },
+  data() {
+    return {
+      accepted: false,
+      title: 'Visitor',
+      change: '',
+      visitor: 1
+    }
   }
 }
 </script>
